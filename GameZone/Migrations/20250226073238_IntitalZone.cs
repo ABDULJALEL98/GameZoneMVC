ef073a4e0787cc2e83b912e10a7240e0af50f7db
ABDUL JALEL ALISMAAEL <97349470+ABDULJALEL98@users.noreply.github.com>
2025-02-26 11:04:43 +0300

    Create New Database

---
+++ GameZone/Migrations/20250226073238_IntitalZone.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameZone.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntitalZone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Adventure");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "Film");
+
+            migrationBuilder.UpdateData(
+                table: "Devices",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Icon",
+                value: "bi bi-playstation");
+
+            migrationBuilder.UpdateData(
+                table: "Devices",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Icon", "Name" },
+                values: new object[] { "bi bi-nintendo-switch", "Nintendo Switch" });
+
+            migrationBuilder.UpdateData(
+                table: "Devices",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "PC");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Adveture");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "Filim");
+
+            migrationBuilder.UpdateData(
+                table: "Devices",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Icon",
+                value: "bi bi-PlayStation");
+
+            migrationBuilder.UpdateData(
+                table: "Devices",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Icon", "Name" },
+                values: new object[] { "bi bi-Nintedo-Swtich", "Nintedo Swtich" });
+
+            migrationBuilder.UpdateData(
+                table: "Devices",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Pc");
+        }
+    }
+}
